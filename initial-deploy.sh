9a6c4f2b69b7ec85cec9857fc4f4b42a6922be29
Craig Miller <craigmiller160@gmail.com>
2023-08-05 10:13:55 -0400

    Setup script

--- initial-deploy.sh
+++ initial-deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-helm install cert-manager ./deploy/chart --kube-context=microk8s --wait --timeout 5m --namespace infra-prod --values ./deploy/chart/values.yml --set cert-manager.installCRDs=true
+if [ $# -ne 1 ]; then
+  echo "Must provide command"
+  exit 1
+fi
+
+helm $1 \
+  cert-manager \
+  ./deploy/chart \
+  --kube-context=microk8s \
+  --wait \
+  --timeout 5m \
+  --namespace infra-prod \
+  --values ./deploy/chart/values.yml \
+  --set cert-manager.installCRDs=true
